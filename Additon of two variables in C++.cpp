311ff753f0c2d3a483c3c254d46756ca11518b69
mr030804 <159411718+mr030804@users.noreply.github.com>
2024-02-20 15:10:11 +0530

    Add files via upload
    Addition of two or more numbers using C++

---
+++ Additon of two variables in C++.cpp
@@ -0,0 +1,17 @@
+#include <cmath>
+#include <cstdio>
+#include <vector>
+#include <iostream>
+#include <algorithm>
+using namespace std;
+
+
+int main() {
+    int a, b, c, sum;
+    cin >>a; 
+    cin >>b;
+    cin >>c; 
+    sum = a+b+c;
+    cout <<sum;
+     return 0;
+}
